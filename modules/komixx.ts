4ec23aacbe2c686d63e376371afb802b5237abae
sleepycrow <sleepycrow@posteo.net>
2026-02-20 02:11:12 +0100

    Remove dependency on jSDOM

--- modules/komixx.ts
+++ modules/komixx.ts
@@ -1,19 +1,18 @@
 import { MatrixEvent, Room } from "matrix-js-sdk";
 import { ModuleDefinition } from "../interfaces/ModuleDefinition.d.ts";
 import { client } from "../client.ts";
-import { JSDOM } from 'jsdom';
 
 const BASE_URL = 'https://komixxy.pl';
+const IMAGE_REGEX = /src=\"(\/uimages\/[^.]+\.(?:jpg|jpeg|png))\"/gi;
 
 async function getImageUrl(): Promise<string> {
 	const resp = await fetch(`${BASE_URL}/losuj`);
 	const rawText = await resp.text();
-	const dom = new JSDOM(rawText);
 
-	const imgUrl = dom.window.document.querySelector('.picwrapper img')?.getAttribute('src');
-	if (!imgUrl) throw new Error('could not retrieve image');
+	const matches = IMAGE_REGEX.exec(rawText);
+	if (!matches || !matches[1]) throw new Error('could not retrieve image');
 
-	return BASE_URL + imgUrl;
+	return BASE_URL + matches[1];
 }
 
 const definition: ModuleDefinition = {
@@ -28,9 +27,10 @@ const definition: ModuleDefinition = {
 			const filename = imgUrl.split('/').pop() as string;
 			const fileFetch = await fetch(imgUrl);
 			const fileBlob = await fileFetch.blob();
+
+			if (fileBlob.type.substring(0, 5) !== 'image') throw new Error('could not retrieve image');
 			
 			const { content_uri } = await client.uploadContent(fileBlob, { name: filename });
-
 			client.sendImageMessage(room.roomId, content_uri, { mimetype: fileBlob.type }, filename);
 		},
 	}],
